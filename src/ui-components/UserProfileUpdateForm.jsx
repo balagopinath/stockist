1b16568e264c2a466e5eb9c0a524c5c6050ff7e5
Gopinath Balasubramaniam <balagopinath@hotmail.com>
2024-02-08 22:41:06 +0530

    Compiling app in react 20 version

---
+++ src/ui-components/UserProfileUpdateForm.jsx
@@ -0,0 +1,256 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { Button, Flex, Grid, TextField } from "@aws-amplify/ui-react";
+import { fetchByPath, getOverrideProps, validateField } from "./utils";
+import { API } from "aws-amplify";
+import { getUserProfile } from "../graphql/queries";
+import { updateUserProfile } from "../graphql/mutations";
+export default function UserProfileUpdateForm(props) {
+  const {
+    Id: IdProp,
+    userProfile: userProfileModelProp,
+    onSuccess,
+    onError,
+    onSubmit,
+    onValidate,
+    onChange,
+    overrides,
+    ...rest
+  } = props;
+  const initialValues = {
+    Id: "",
+    userId: "",
+    userName: "",
+  };
+  const [Id, setId] = React.useState(initialValues.Id);
+  const [userId, setUserId] = React.useState(initialValues.userId);
+  const [userName, setUserName] = React.useState(initialValues.userName);
+  const [errors, setErrors] = React.useState({});
+  const resetStateValues = () => {
+    const cleanValues = userProfileRecord
+      ? { ...initialValues, ...userProfileRecord }
+      : initialValues;
+    setId(cleanValues.Id);
+    setUserId(cleanValues.userId);
+    setUserName(cleanValues.userName);
+    setErrors({});
+  };
+  const [userProfileRecord, setUserProfileRecord] =
+    React.useState(userProfileModelProp);
+  React.useEffect(() => {
+    const queryData = async () => {
+      const record = IdProp
+        ? (
+            await API.graphql({
+              query: getUserProfile.replaceAll("__typename", ""),
+              variables: { Id: IdProp },
+            })
+          )?.data?.getUserProfile
+        : userProfileModelProp;
+      setUserProfileRecord(record);
+    };
+    queryData();
+  }, [IdProp, userProfileModelProp]);
+  React.useEffect(resetStateValues, [userProfileRecord]);
+  const validations = {
+    Id: [{ type: "Required" }],
+    userId: [{ type: "Required" }],
+    userName: [{ type: "Required" }],
+  };
+  const runValidationTasks = async (
+    fieldName,
+    currentValue,
+    getDisplayValue
+  ) => {
+    const value =
+      currentValue && getDisplayValue
+        ? getDisplayValue(currentValue)
+        : currentValue;
+    let validationResponse = validateField(value, validations[fieldName]);
+    const customValidator = fetchByPath(onValidate, fieldName);
+    if (customValidator) {
+      validationResponse = await customValidator(value, validationResponse);
+    }
+    setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }));
+    return validationResponse;
+  };
+  return (
+    <Grid
+      as="form"
+      rowGap="15px"
+      columnGap="15px"
+      padding="20px"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        let modelFields = {
+          Id,
+          userId,
+          userName,
+        };
+        const validationResponses = await Promise.all(
+          Object.keys(validations).reduce((promises, fieldName) => {
+            if (Array.isArray(modelFields[fieldName])) {
+              promises.push(
+                ...modelFields[fieldName].map((item) =>
+                  runValidationTasks(fieldName, item)
+                )
+              );
+              return promises;
+            }
+            promises.push(
+              runValidationTasks(fieldName, modelFields[fieldName])
+            );
+            return promises;
+          }, [])
+        );
+        if (validationResponses.some((r) => r.hasError)) {
+          return;
+        }
+        if (onSubmit) {
+          modelFields = onSubmit(modelFields);
+        }
+        try {
+          Object.entries(modelFields).forEach(([key, value]) => {
+            if (typeof value === "string" && value === "") {
+              modelFields[key] = null;
+            }
+          });
+          await API.graphql({
+            query: updateUserProfile.replaceAll("__typename", ""),
+            variables: {
+              input: {
+                Id: userProfileRecord.Id,
+                ...modelFields,
+              },
+            },
+          });
+          if (onSuccess) {
+            onSuccess(modelFields);
+          }
+        } catch (err) {
+          if (onError) {
+            const messages = err.errors.map((e) => e.message).join("\n");
+            onError(modelFields, messages);
+          }
+        }
+      }}
+      {...getOverrideProps(overrides, "UserProfileUpdateForm")}
+      {...rest}
+    >
+      <TextField
+        label="Id"
+        isRequired={true}
+        isReadOnly={true}
+        value={Id}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              Id: value,
+              userId,
+              userName,
+            };
+            const result = onChange(modelFields);
+            value = result?.Id ?? value;
+          }
+          if (errors.Id?.hasError) {
+            runValidationTasks("Id", value);
+          }
+          setId(value);
+        }}
+        onBlur={() => runValidationTasks("Id", Id)}
+        errorMessage={errors.Id?.errorMessage}
+        hasError={errors.Id?.hasError}
+        {...getOverrideProps(overrides, "Id")}
+      ></TextField>
+      <TextField
+        label="User id"
+        isRequired={true}
+        isReadOnly={false}
+        value={userId}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              Id,
+              userId: value,
+              userName,
+            };
+            const result = onChange(modelFields);
+            value = result?.userId ?? value;
+          }
+          if (errors.userId?.hasError) {
+            runValidationTasks("userId", value);
+          }
+          setUserId(value);
+        }}
+        onBlur={() => runValidationTasks("userId", userId)}
+        errorMessage={errors.userId?.errorMessage}
+        hasError={errors.userId?.hasError}
+        {...getOverrideProps(overrides, "userId")}
+      ></TextField>
+      <TextField
+        label="User name"
+        isRequired={true}
+        isReadOnly={false}
+        value={userName}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              Id,
+              userId,
+              userName: value,
+            };
+            const result = onChange(modelFields);
+            value = result?.userName ?? value;
+          }
+          if (errors.userName?.hasError) {
+            runValidationTasks("userName", value);
+          }
+          setUserName(value);
+        }}
+        onBlur={() => runValidationTasks("userName", userName)}
+        errorMessage={errors.userName?.errorMessage}
+        hasError={errors.userName?.hasError}
+        {...getOverrideProps(overrides, "userName")}
+      ></TextField>
+      <Flex
+        justifyContent="space-between"
+        {...getOverrideProps(overrides, "CTAFlex")}
+      >
+        <Button
+          children="Reset"
+          type="reset"
+          onClick={(event) => {
+            event.preventDefault();
+            resetStateValues();
+          }}
+          isDisabled={!(IdProp || userProfileModelProp)}
+          {...getOverrideProps(overrides, "ResetButton")}
+        ></Button>
+        <Flex
+          gap="15px"
+          {...getOverrideProps(overrides, "RightAlignCTASubFlex")}
+        >
+          <Button
+            children="Submit"
+            type="submit"
+            variation="primary"
+            isDisabled={
+              !(IdProp || userProfileModelProp) ||
+              Object.values(errors).some((e) => e?.hasError)
+            }
+            {...getOverrideProps(overrides, "SubmitButton")}
+          ></Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  );
+}
